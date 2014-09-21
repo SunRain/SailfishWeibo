bf98be4458d4c17bb54f98359ba25f22ccc46693
WangGuojian <wanggjghost@gmail.com>
2014-09-21 19:23:56 +0800

    use my apk key

--- src/app.h
+++ src/app.h
@@ -5,8 +5,8 @@
 #define Q_PROCESSOR_X86
 
 //TODO:使用了https://github.com/WeCase的api key
-static const char *APPKEY = "1011524190";
-static const char *APPSECRET = "1898b3f668368b9f4a6f7ac8ed4a918f";
+static const char *APPKEY = "4091495491"; //"1011524190";
+static const char *APPSECRET = "4700ee38f5ecc0398074b669b8412b3a"; /*"1898b3f668368b9f4a6f7ac8ed4a918f";*/
 
 
 
