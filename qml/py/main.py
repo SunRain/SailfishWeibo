166ce2dd0b2068e9602a2e44f6d37d1d3c8e37e7
0312birdzhang <0312birdzhang@gmail.com>
2016-01-06 14:35:56 +0800

    添加异常判断

--- qml/py/main.py
+++ qml/py/main.py
@@ -1,10 +1,14 @@
 import rpweibo
 import pyotherside
-
 def getToken(API_KEY,API_SECRET,REDIRECT_URI,username,password):
     example_app = rpweibo.Application(API_KEY, API_SECRET, REDIRECT_URI)
     weibo = rpweibo.Weibo(example_app)
     authenticator = rpweibo.UserPassAutheticator(username, password)
-    access_token = authenticator.auth(example_app)
+    try:
+        weibo.auth(authenticator)
+        access_token = authenticator.auth(example_app)
+    except rpweibo.AuthorizeFailed:
+        print("Invalid username or password!")
+        pyotherside.send("pyhandle","Error")
     print(access_token)
     pyotherside.send("pyhandle",access_token)
